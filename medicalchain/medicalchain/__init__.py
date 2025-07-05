90e6ec7ceecf33c1af467af56b2f15b28bbb16d8
fanuel045 <madela.fanuel.1@gmail.com>
2025-07-05 14:17:20 +0200

    🌿 Structure complète du projet : backend, frontend et static

--- medicalchain/medicalchain/__init__.py
+++
@@ -1,6 +0,0 @@
-# from __future__ import absolute_import, unicode_literals
-
-# # # Cette ligne importe et initialise Celery au démarrage du projet.
-# from .celery import app as celery_app
-
-# __all__ = "celery_app"
--- medicalchain/medicalchain/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "medicalchain.settings.dev"
-)  # Changé à dev
-application = get_wsgi_application()
